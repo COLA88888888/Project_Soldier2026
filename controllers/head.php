1a79293341550487de177a0a0d3763b484e4b70d
lakhamtha-sudo <>
2026-06-08 15:34:56 +0700

    update

--- controllers/head.php
+++ controllers/head.php
@@ -39,8 +39,7 @@ if (!isset($_SESSION['user_id']) && !isset($_SESSION['role'])) {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>ຫ້ອງການກົມໃຫຍ່ການເມືອງ</title>
 
-<!-- Google Font: Source Sans Pro -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Google Font removed (local Lao fonts used instead for faster loading) -->
 <!-- Font Awesome -->
 <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
 <!-- Ionicons -->
@@ -48,25 +47,11 @@ if (!isset($_SESSION['user_id']) && !isset($_SESSION['role'])) {
 <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
 <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
 <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-
-<!-- Tempusdominus Bootstrap 4 -->
-<link rel="stylesheet" href="../../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-<!-- iCheck -->
-<link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-<!-- JQVMap -->
-
-<link rel="stylesheet" href="../../plugins/jqvmap/jqvmap.min.css">
-<!-- Theme style -->
+<!-- Theme style (AdminLTE) -->
 <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
 <!-- overlayScrollbars -->
 <link rel="stylesheet" href="../../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-<!-- Daterange picker -->
- 
-<link rel="stylesheet" href="../../plugins/daterangepicker/daterangepicker.css">
-<!-- summernote -->
-
-<link rel="stylesheet" href="../../plugins/summernote/summernote-bs4.min.css">
+<!-- Ionicons (local) -->
 <link rel="stylesheet" href="../../ionicons-2.0.1/css/ionicons.min.css">
 
 <script src="../../sweetalert/jquery-3.6.0.js"></script>
@@ -179,10 +164,7 @@ if (!isset($_SESSION['user_id']) && !isset($_SESSION['role'])) {
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
-<!-- Preloader -->
-<div class="preloader flex-column justify-content-center align-items-center">
-<img class="animation__shake" src="../../logo/1.jfif" alt="AdminLTELogo" height="60" width="60">
-</div>
+<!-- Preloader removed for faster page navigation -->
 
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
